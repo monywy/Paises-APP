4030eb83d6ec873442f6d2c6b7ada07bbabbf04d
monywy <44063035+monywy@users.noreply.github.com>
2022-10-10 15:49:01 -0500

    1 file modified

--- src/app/pais/services/pais.service.ts
+++ src/app/pais/services/pais.service.ts
@@ -16,6 +16,12 @@ export class PaisService {
   buscarPais( termino:string ): Observable<any>{
     const url = `${this.apiUrl}/name/${ termino }`
     return this.http.get(url);
+    ///Se puede realizar una validación directamente desde la petición para atrapar el error con .pipe
+
+    // return this.http.get(url)
+    // .pipe(
+    //  catchError ( err => of([])) //atrapa del tipo observable y retorna un arreglo vacío
+    // )
 
   }
 
