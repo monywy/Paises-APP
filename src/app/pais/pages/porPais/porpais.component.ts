b04fa677cb0a9d2473a35c7aa24b2c45c2292953
monywy <44063035+monywy@users.noreply.github.com>
2022-10-10 15:41:36 -0500

    2 files modified

--- src/app/pais/pages/porPais/porpais.component.ts
+++ src/app/pais/pages/porPais/porpais.component.ts
@@ -7,18 +7,27 @@ import { PaisService } from '../../services/pais.service';
   styleUrls: ['./porpais.component.css']
 })
 export class PorpaisComponent {
+//  Manejo de errores de la peticion a la API
+   hayError: boolean = false;
 
   termino: string = '';
 
   constructor(private paisservice: PaisService) { }
 
   buscar(){
+    //sino hay un error
+    this.hayError = false;
     console.log(this.termino);
     this.paisservice.buscarPais(this.termino)
     .subscribe ( resp => {
       console.log(resp);
 
-    });
+    },(err) => {
+    //metodo para controlar errores con .suscribe 
+      this.hayError = true;
+
+      }); 
+
   }
 
 }
