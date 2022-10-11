dcdbe830a55b1f2339efc1e162f218e12d467b8b
monywy <44063035+monywy@users.noreply.github.com>
2022-10-11 17:43:39 -0500

    2 files modified, 1 deleted

--- src/app/pais/pages/porRegion/porregion.component.ts
+++ src/app/pais/pages/porRegion/porregion.component.ts
@@ -3,9 +3,18 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-porregion',
   templateUrl: './porregion.component.html',
-  styleUrls: ['./porregion.component.css']
+  styles: [
+    `
+    button{
+      margin-right: 5px;
+      border-color: pink;
+    }
+    `
+  ]
 })
 export class PorregionComponent implements OnInit {
+
+  regiones: string[] = [ 'Africa', 'Americas', 'Asia', 'Europe', 'Oceania'];
 
   constructor() { }
 
