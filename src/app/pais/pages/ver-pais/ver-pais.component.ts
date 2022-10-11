edbc21217fbb5ad54dfba69ce83abf1667a28947
monywy <44063035+monywy@users.noreply.github.com>
2022-10-11 12:53:34 -0500

    1 file modified

--- src/app/pais/pages/ver-pais/ver-pais.component.ts
+++ src/app/pais/pages/ver-pais/ver-pais.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { PaisService } from '../../services/pais.service';
 
 @Component({
   selector: 'app-ver-pais',
@@ -9,13 +10,21 @@ import { ActivatedRoute } from '@angular/router';
 export class VerPaisComponent implements OnInit {
 
   //Para suscribirnos a cualquier cambio del URL
-  constructor(private activatedRounter:ActivatedRoute) { }
+  constructor(
+    private activatedRounter:ActivatedRoute,
+    private paisServices: PaisService
+    ) { }
   
   //Se ejecuita cuando el componente esta inicializado
   ngOnInit():void {
     this.activatedRounter.params
     .subscribe( ({ id }) =>{
       console.log(id);
+
+      this.paisServices.getPaisPorAlpha( id )
+      .subscribe(pais => {
+        console.log(pais);
+      })
     });
   }
 
