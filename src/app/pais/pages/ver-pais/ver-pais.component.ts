b882427650f1cf1e75760cec783b8853818e03ef
monywy <44063035+monywy@users.noreply.github.com>
2022-10-11 13:03:52 -0500

    1 file modified

--- src/app/pais/pages/ver-pais/ver-pais.component.ts
+++ src/app/pais/pages/ver-pais/ver-pais.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { PaisService } from '../../services/pais.service';
+import { switchMap }  from 'rxjs/operators'; //Permite recibir un observable y regresar un observable
 
 @Component({
   selector: 'app-ver-pais',
@@ -17,8 +18,8 @@ export class VerPaisComponent implements OnInit {
   
   //Se ejecuita cuando el componente esta inicializado
   ngOnInit():void {
-    this.activatedRounter.params
-    //Manejos de valores dependientes de un observable
+    //Manejos de valores dependientes de un observable Método 1
+    /*this.activatedRounter.params
     .subscribe( ({ id }) =>{
       console.log(id);
 
@@ -26,7 +27,20 @@ export class VerPaisComponent implements OnInit {
       .subscribe(pais => {
         console.log(pais);
       })
-    });
+    });*/
+
+    ///////////////////////////////////////////////Método 2
+
+     this.activatedRounter.params
+     //Uso de operadores de rxjs
+     .pipe(
+       //Recibe el valor del observable anterior y retorna un nuevo observable
+       switchMap( (params)=> this.paisServices.getPaisPorAlpha(params.id) )
+     )
+     .subscribe(resp=>{
+      console.log(resp)
+     });
+
   }
 
 }
