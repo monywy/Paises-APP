bdeb14a16a8a9ab2fc52b6c03dae4f2bb27a55d7
monywy <44063035+monywy@users.noreply.github.com>
2022-10-11 11:40:26 -0500

    2 files modified, 1 deleted

--- src/app/pais/pages/por-Capital/por-capital.component.ts
+++ src/app/pais/pages/por-Capital/por-capital.component.ts
@@ -1,15 +1,47 @@
 import { Component, OnInit } from '@angular/core';
+import { Country } from '../../interfaces/pais.interface';
+import { PaisService } from '../../services/pais.service';
 
 @Component({
   selector: 'app-por-capital',
   templateUrl: './por-capital.component.html',
-  styleUrls: ['./por-capital.component.css']
+  styleUrls: []
 })
-export class PorCapitalComponent implements OnInit {
+export class PorCapitalComponent {
+  //Manejo de errores de la peticion a la API
+  hayError: boolean = false;
+  termino: string = '';
 
-  constructor() { }
+  //Mostrar información en la tabla
+  paises : Country[] = [];
 
-  ngOnInit() {
+  constructor(private paisservice: PaisService) { }
+
+  buscar( termino: string){
+    //sino hay un error
+    this.hayError = false;
+    this.termino = termino;
+   // console.log(this.termino);
+    this.paisservice.buscarPais(termino)
+    .subscribe ( paises => {
+      console.log(paises);
+      this.paises= paises;
+
+    },(err) => {
+    //metodo para controlar errores con .suscribe 
+      this.hayError = true;
+      this.paises = [];
+      }); 
+
   }
+
+  //Método para mostrar las sugerencias conforme voy escribiendo
+  sugerencias(termino: string){
+    //validación de errores
+    this.hayError = false;
+
+  }
+
+  
 
 }
