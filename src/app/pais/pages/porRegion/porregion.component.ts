517dbe021dc5fb6dbec79b84cb1dbb8d2a5a15e5
monywy <44063035+monywy@users.noreply.github.com>
2022-10-11 23:40:32 -0500

    2 files modified

--- src/app/pais/pages/porRegion/porregion.component.ts
+++ src/app/pais/pages/porRegion/porregion.component.ts
@@ -9,20 +9,22 @@ import { Component, OnInit } from '@angular/core';
       margin-right: 5px;
       border-color: pink;
     }
-    `
-  ]
+    `,
+  ],
 })
 export class PorregionComponent {
-
-  regiones: string[] = [ 'Africa', 'Americas', 'Asia', 'Europe', 'Oceania'];
+  regiones: string[] = ['Africa', 'Americas', 'Asia', 'Europe', 'Oceania'];
   regionActiva: string = '';
 
-  constructor() { }
+  constructor() {}
 
-  activarRegion(region: string){
-    this.regionActiva = region;
+  //Manejo de clases CSS condicionales para control de interfaz gráfica de busquedas por region
+
+  getClaseCSS (region: string) : string{
+    return (region === this.regionActiva) ? 'btn btn-primary' : 'btn btn-outline-primary';
   }
 
-  
-
+  activarRegion(region: string) {
+    this.regionActiva = region;
+  }
 }
